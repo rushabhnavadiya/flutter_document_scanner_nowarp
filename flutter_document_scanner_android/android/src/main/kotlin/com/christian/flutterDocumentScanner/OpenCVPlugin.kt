c894cec0729a5ab6421136685a524a83a1fcb9f7
Rushabh Navadiya <rushabhnavadiya1998@gmail.com>
2025-02-01 16:18:53 +0530

    Update OpenCVPlugin.kt

--- flutter_document_scanner_android/android/src/main/kotlin/com/christian/flutterDocumentScanner/OpenCVPlugin.kt
+++ flutter_document_scanner_android/android/src/main/kotlin/com/christian/flutterDocumentScanner/OpenCVPlugin.kt
@@ -15,7 +15,7 @@ class OpenCVPlugin {
         ) {
             try {
                 // val src = Imgcodecs.imdecode(MatOfByte(*byteData), Imgcodecs.IMREAD_UNCHANGED)
-                val src = Imgcodecs.mdecode(MatOfByte(*byteData), Imgcodecs.IMREAD_COLOR)
+                val src = Imgcodecs.imdecode(MatOfByte(*byteData), Imgcodecs.IMREAD_COLOR)
 
                 val documentContour = findBiggestContour(src, minContourArea)
 
@@ -176,7 +176,7 @@ class OpenCVPlugin {
         ) {
             try {
                 // val src = Imgcodecs.imdecode(MatOfByte(*byteData), Imgcodecs.IMREAD_UNCHANGED)
-                val src = Imgcodecs.mdecode(MatOfByte(*byteData), Imgcodecs.IMREAD_COLOR)
+                val src = Imgcodecs.imdecode(MatOfByte(*byteData), Imgcodecs.IMREAD_COLOR)
                 val documentContour = MatOfPoint(
                     Point("${points[0]["x"]}".toDouble(), "${points[0]["y"]}".toDouble()),
                     Point("${points[1]["x"]}".toDouble(), "${points[1]["y"]}".toDouble()),
@@ -233,7 +233,7 @@ class OpenCVPlugin {
                     else -> FilterType.Natural
                 }
                 // val src = Imgcodecs.imdecode(MatOfByte(*byteData), Imgcodecs.IMREAD_UNCHANGED)
-                val src = Imgcodecs.mdecode(MatOfByte(*byteData), Imgcodecs.IMREAD_COLOR)
+                val src = Imgcodecs.imdecode(MatOfByte(*byteData), Imgcodecs.IMREAD_COLOR)
 
                 var dstEnd = Mat()
 
